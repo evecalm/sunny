41e9e32fbb0799e03578e628a62475b9942b64cb
evecalm.mi <evecalm@live.com>
2013-12-05 10:33:34 +0800

    add browser detection

--- assets/main.js
+++ assets/main.js
@@ -1,44 +1,51 @@
-var music = document.getElementById('music'),
-	musicControl = document.getElementById('music-control'),
-	timeCount = document.getElementById('time-cunt'),
-	beginTime = new Date(2013,8,21,0,21,12);
+;(function (window) {
+	var music = document.getElementById('music'),
+		musicControl = document.getElementById('music-control'),
+		timeCount = document.getElementById('time-cunt'),
+		beginTime = new Date(2013,8,21,0,21,12);
 
-musicControl.addEventListener('click',function () {
-	if (music.paused) {
-		music.play();
-		musicControl.classList.remove('music-slient');
-	} else {
-		music.pause();
-		musicControl.classList.add('music-slient');
+	if (!music || !music.canPlayType) {
+		document.body.innerText = 'Please use browser support html5, such as Chrome, Firefox, Safari.'
+		return;
 	}
-},false);
 
-function cuntTime () {
-	var str = '',
-		tmp = 0;
-		stamp = (+new Date()) - beginTime;
-	stamp = Math.floor(stamp / 1000);
-	// year
-	tmp = Math.floor(stamp / (60 * 60 * 24 * 365));
-	str += tmp + (tmp > 1 ? ' years ' : ' year ');
-	// day
-	stamp -= tmp * 60 * 60 * 24 * 365;
-	tmp = Math.floor(stamp / (60 * 60 * 24));
-	str += tmp + (tmp > 1 ? ' days ' : ' day ');
-	// hour
-	stamp -= tmp * 60 * 60 * 24;
-	tmp = Math.floor(stamp / (60 * 60));
-	str += tmp + (tmp > 1 ? ' hours ' : ' hour ');
-	// minute
-	stamp -= tmp * 60 * 60;
-	tmp = Math.floor(stamp / 60);
-	str += tmp + (tmp > 1 ? ' minutes ' : ' minute ');
-	// second
-	stamp -= tmp * 60;
-	tmp = stamp;
-	str += tmp + (tmp > 1 ? ' seconds ' : ' second ');
-	timeCount.innerText = str;
-}
+	musicControl.addEventListener('click',function () {
+		if (music.paused) {
+			music.play();
+			musicControl.classList.remove('music-slient');
+		} else {
+			music.pause();
+			musicControl.classList.add('music-slient');
+		}
+	},false);
 
-setInterval(cuntTime, 1000);
+	function cuntTime () {
+		var str = '',
+			tmp = 0;
+			stamp = (+new Date()) - beginTime;
+		stamp = Math.floor(stamp / 1000);
+		// year
+		tmp = Math.floor(stamp / (60 * 60 * 24 * 365));
+		str += tmp + (tmp > 1 ? ' years ' : ' year ');
+		// day
+		stamp -= tmp * 60 * 60 * 24 * 365;
+		tmp = Math.floor(stamp / (60 * 60 * 24));
+		str += tmp + (tmp > 1 ? ' days ' : ' day ');
+		// hour
+		stamp -= tmp * 60 * 60 * 24;
+		tmp = Math.floor(stamp / (60 * 60));
+		str += tmp + (tmp > 1 ? ' hours ' : ' hour ');
+		// minute
+		stamp -= tmp * 60 * 60;
+		tmp = Math.floor(stamp / 60);
+		str += tmp + (tmp > 1 ? ' minutes ' : ' minute ');
+		// second
+		stamp -= tmp * 60;
+		tmp = stamp;
+		str += tmp + (tmp > 1 ? ' seconds ' : ' second ');
+		timeCount.innerText = str;
+	}
+
+	setInterval(cuntTime, 1000);
+})(window);
 
